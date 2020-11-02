b403acda333e489c818c272094d928dcea0d87a2
Peyton Jones <peytonj.dev@gmail.com>
2020-11-02 15:36:58 -0700

    test command line comment

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,8 @@
 import chai from 'chai';
 const expect = chai.expect;
 
+//this is a test comment//
+
 describe('See if the tests are running', function() {
   it('should return true', function() {
     expect(true).to.equal(true);
