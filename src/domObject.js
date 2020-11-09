ab513a63583b0834f3e6abfbb820f49c1714728e
Peyton Jones <peytonj.dev@gmail.com>
2020-11-09 10:44:09 -0700

    created exports within domObject.js

---
+++ src/domObject.js
@@ -0,0 +1,11 @@
+
+// login form and error msg
+export const loginForm = document.querySelector(".login-form-panel");
+export const loginBtn = document.querySelector(".login-btn");
+export const loginErrorMsg = document.querySelector(".login-error-msg-box");
+export const userName = document.querySelector('.username-login-input');
+export const userPassword = document.querySelector('.password-login-input');
+// user dashboard selectors
+export const roomTypeBtn = document.getElementById('room-type-search-btn');
+export const roomDateBtn = document.getElementById('date-search-btn');
+
