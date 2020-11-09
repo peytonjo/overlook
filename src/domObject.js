286197270be25745a8d49ae3a181de1fdcdd1528
Peyton Jones <peytonj.dev@gmail.com>
2020-11-09 15:22:43 -0700

    added userDashboard querySelector

--- src/domObject.js
+++ src/domObject.js
@@ -6,6 +6,7 @@ export const loginErrorMsg = document.querySelector(".login-error-msg-box");
 export const userName = document.querySelector('.username-login-input');
 export const userPassword = document.querySelector('.password-login-input');
 // user dashboard selectors
+export const userDashboard = document.querySelector('.user-dashboard')
 export const roomTypeBtn = document.getElementById('room-type-search-btn');
 export const roomDateBtn = document.getElementById('date-search-btn');
 
