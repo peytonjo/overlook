58354e1b5f5f3681f321ca3106f79b35cd6f9867
Peyton Jones <peytonj.dev@gmail.com>
2020-11-09 21:11:47 -0700

    add login functionality

--- src/index.js
+++ src/index.js
@@ -1,43 +1,85 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS (SCSS) file
 import { getData } from './apiCalls';
 import './css/base.scss';
-import './Room'
-import './User'
-import './domObject'
+import './classes/Room'
+import './classes/User'
+import './domElements'
+import { loginBtn, loginForm, managerDashboard, userDashboard, userName, userPassword } from './domElements';
+import User from './classes/User';
+import Manager from './manager';
+
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
-// import User from './User';
+
+ //-----------------------------populate storage-----------------------
+
+const getUsers = () => {
+  return getData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+    .then((usersData) => {
+      return usersData.users.map((data) => {
+        return new User(data);
+      })
+    })
+    .catch(error => console.log(error));
+}
 
 
+const populateStorage = () => {
+  getUsers().then(data => localStorage.setItem('users', JSON.stringify(data)))
+}
 
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = userName.value;
-    const password = userPassword.value;
-    console.log(loginForm)
-    const userID = user.id
+populateStorage()
 
-    if (username === `customer${userID}` && password === "overlook2020") {
-        alert("You have successfully logged in!");
-    } else {
-        loginErrorMsg.remove('.hidden')
+//-------------------toggle page view functions-----------
+
+const displayManagerDashboard = () => {
+  managerDashboard.classList.remove('hidden')
+  loginForm.classList.add('hidden')
+  console.log('currentUser', localStorage.getItem('currentUser'))
+}
+
+const displayUserDashboard = () => {
+  userDashboard.classList.remove('hidden')
+  loginForm.classList.add('hidden')
+}
+
+//------------------------- login functionality -------------------
+const validateLoginInputs = () => {
+  if (userName.value === 'manager' && userPassword.value === 'overlook2020') {
+    const manager = new Manager({id: 0, name: 'admin'})
+    manager.loggedIn = true
+    localStorage.setItem('currentUser', JSON.stringify(manager))
+    displayManagerDashboard()
+  } else {
+    const loginPrefix = userName.value.split('').slice(0,8).join('')
+    const id = userName.value.split('').slice(8).join('')
+    const matchingUser = findUser(id)
+    if (matchingUser && loginPrefix === 'customer' && userPassword.value === 'overlook2020') {
+      matchingUser.loggedIn = true
+      localStorage.setItem('currentUser', JSON.stringify(matchingUser))
+      displayUserDashboard()
     }
+  }
+}
+
+const findUser = (id) => {
+  const usersData = JSON.parse(localStorage.getItem('users'))  
+
+  return usersData.find(user => {
+    return user.id === parseInt(id)
+  })
+}
+
+
+// -----------------------------event listeners---------------------------
+loginBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  
+  validateLoginInputs()
 })
-
-
-
-
-
-
-// loginBtn.addEventListener('click', () => {
-//   if(){
-
-//   }
-// })
 
 
 // roomTypeBtn.addEventListener('click', () => {
@@ -76,12 +118,3 @@ loginBtn.addEventListener("click", (e) => {
 //   })
 // })
 
-// function changePageView(elements) {
-//   elements.forEach(element => {
-//     if (element.addHidden) {
-//       (element.name).classList.add('hidden');
-//     } else {
-//       (element.name).classList.remove('hidden');
-//     }
-//   })
-// }
