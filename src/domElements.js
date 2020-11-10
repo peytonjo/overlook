9225f03733011e4164e1e72b0d42f09b125ec27e
Peyton Jones <peytonj.dev@gmail.com>
2020-11-09 21:06:10 -0700

    move dom elements to seperate file

--- src/domElements.js
+++ src/domElements.js
@@ -5,6 +5,8 @@ export const loginBtn = document.querySelector(".login-btn");
 export const loginErrorMsg = document.querySelector(".login-error-msg-box");
 export const userName = document.querySelector('.username-login-input');
 export const userPassword = document.querySelector('.password-login-input');
+//manager dashboard selectors
+export const managerDashboard = document.querySelector('.manager-dashboard')
 // user dashboard selectors
 export const userDashboard = document.querySelector('.user-dashboard')
 export const roomTypeBtn = document.getElementById('room-type-search-btn');
